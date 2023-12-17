dbeae15e5b90f6604579b8ebb082e9a4303f52aa
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2023-12-17 12:36:25 +0100

    data servis

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,5 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import {
+  collection,
+  Firestore,
+  collectionData,
+  doc,
+  deleteDoc, 
+  addDoc
+  } from '@angular/fire/firestore';
+
+  export interface Kontakt {
+    id?: number;
+    date: string;
+    done: boolean;
+    name: string;
+    category: string;
+    }
+
 @Injectable({
   providedIn: 'root'
 })
